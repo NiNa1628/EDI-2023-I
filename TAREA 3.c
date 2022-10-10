fae89046af15c18cb526ed741972705747b9c92a
NiNa <111335476+NiNa1628@users.noreply.github.com>
2022-10-09 22:34:08 -0500

    Add files via upload

--- TAREA 3.c
+++ TAREA 3.c
@@ -1,23 +1,89 @@
-
-
+//González Medina Claudia Karina
 
 #include <stdio.h>
+#define SIZE 10
+int pideN();
+int pideSearch();
+
+void pideArreglo(int array[SIZE], int n);
+int valorCiclo(int array[SIZE], int n, int search);
+int valorRecursivo(int array[SIZE], int n, int search);
+
 int main()
 {
-int a = 8, b = 3, c = -5, primero, segundo, tercero, cuarto;
-float x = 88, y = 3.5, z = -5.2;
-int i = 8, j = 5;
+    int laN;
+    int search;
+    int arreglo[SIZE]; //{4, 9, 3, 15, 93, 8, 11, 7, 14, 12}
+    laN=pideN();
+    search=pideSearch();
+    pideArreglo(arreglo, laN);
+    valorCiclo(arreglo, laN, search);
+    int val;
+    valorRecursivo(arreglo, laN, search);
+}
 
-primero=2 * b + 3 * (a - c);
-printf("primero: %d\n", primero);
+int pideN()
+{
+    int n;
+    do{
+    printf("No. of fact: ");
+    scanf("%d", &n);
+    }while(n<1||n>SIZE);
+    return(n);
+}
 
-segundo=a * (c % b);
-printf("segundo: %d\n", segundo);
+int pideSearch()
+{
+    int search;
+    printf("Give me a number of search: ");
+    scanf("%d", &search);
+}
 
-tercero=2*y+3*(x-z);
-printf("tercero: %.2f\n", tercero);
+void pideArreglo(int array[SIZE], int n)
+{
+    int cont;
+    for(cont = 0; cont<=n; cont++)
+    {
+        printf("Give me a number of fact %d: ", cont);
+        scanf("%d", &array[cont]);
+    }
+}
 
-cuarto=2 * ((i / 5) + (4 * (j - 3)) % ( i + j - 2));
-printf("cuarto: %d\n", cuarto);
+int valorCiclo(int array[SIZE],int n, int search)
+{
+    int left=0, right=n, mean, band=0;
+    do{
+        mean=(left+right)/2;
+        if(array[mean]==search)
+            band=1;
+        else
+            if(search<array[mean])
+            {
+                right=mean-1;
+                band=1;
+            }
+            else
+            {
+                left=mean+1;
+                band=1;
+            }
+    }while(left<=right && band==0);
+        printf("Results: %d\n", band);
+}
 
+int valorRecursivo(int array[SIZE], int n, int search)
+{
+    int left=0, right=n, mean, band=0;
+    do{
+        mean=(left+right)/2;
+        if(array[mean]==search)
+            band=1;
+        else
+        band=0;
+            if(search<array[mean])
+                band=valorRecursivo(n+1);
+            else
+                band=valorRecursivo(n-1);
+     }while(left<=right && band==0);
+    printf("Results: %d\n", band);
 }
