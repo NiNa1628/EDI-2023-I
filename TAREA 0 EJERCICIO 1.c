7c3706729b2b34dc9e76543263f428064b7ab585
NiNa <111335476+NiNa1628@users.noreply.github.com>
2022-10-31 23:10:28 -0600

    Add files via upload

--- TAREA 0 EJERCICIO 1.c
+++ TAREA 0 EJERCICIO 1.c
@@ -1,27 +1,121 @@
-
 //González Medina Claudia Karina
+/*1.1.Definir una estructura que guarde los datos de cada canción:
+•Código de la canción (numérico)
+•Nombre de la canción
+•Nombre del albúm
+•Número de interpretes
+•Nombre del/de los interpretes (1 a 3 máximo)
+•Número de reproducciones*/
+
 #include <stdio.h>
+#define SIZE 5
+
+struct CANCION
+{
+    int codCancion;
+    char nomCancion[50];
+    char nomAlbum[50];
+    int numInterpretes;
+    char nomInter1[50];
+    char nomInter2[50];
+    char nomInter3[50];
+    int numReproducciones;
+};
+
+void leeCancion(struct CANCION *n);
+void leerArreglo(struct CANCION canciones[]);
+int mayorReproducciones(struct CANCION canciones[]);
+
+
+//1.2.Realice una función que reciba como parámetro una canción y capture todos susdatos
+void leeCancion(struct CANCION *n)
+{
+      fflush(stdin);
+      printf("Dame el Código de la canción (numérico): ");
+      scanf("%d", &n->codCancion);
+      fflush(stdin);
+      printf("Dame el Nombre de la canción: ");
+      scanf("%[^\n]", n->nomCancion);
+      fflush(stdin);
+      printf("Dame el Nombre del albúm: ");
+      scanf("%[^\n]", n->nomAlbum);
+      fflush(stdin);
+      printf("Dame el Número de interpretes: ");
+      scanf("%d", &n->numInterpretes);
+      fflush(stdin);
+      if(n->numInterpretes==1)
+      {
+          printf("Dame el nombre del interprete 1: ");
+          scanf("%[^\n]", n->nomInter1);
+          fflush(stdin);
+      }
+      else
+        if(n->numInterpretes==2)
+        {
+            printf("Dame el nombre del interprete 1: " );
+            scanf("%[^\n]", n->nomInter1);
+            fflush(stdin);
+            printf("Dame el nombre del interprete 2: " );
+            scanf("%[^\n]", n->nomInter2);
+            fflush(stdin);
+        }
+        else
+            if(n->numInterpretes==3)
+            {
+                printf("Dame el nombre del interprete 1:" );
+                scanf("%[^\n]", n->nomInter1);
+                fflush(stdin);
+                printf("Dame el nombre del interprete 2:" );
+                scanf("%[^\n]", n->nomInter2);
+                fflush(stdin);
+                printf("Dame el nombre del interprete 3:" );
+                scanf("%[^\n]", n->nomInter3);
+                fflush(stdin);
+            }
+            fflush(stdin);
+            printf("Dame el Número de reproducciones: ");
+      scanf("%d", &n->numReproducciones);
+}
+
+
+/*1.3.Realice una función que reciba como parámetro un arreglo de canciones y capture todas las canciones
+(haciendo uso de la función anterior)que el usuario necesite (máximo 100)*/
+void leerArreglo(struct CANCION canciones[SIZE])
+{
+    int cont;
+    for(cont=0; cont<SIZE; cont++)
+    {
+        leeCancion(&canciones[cont]);
+    }
+}
+
+
+/*1.4.Realice una función que reciba como parámetro un arreglo de canciones ya capturadas y regrese la
+posición en el arreglo en donde se encuentra la canción con más reproducciones.*/
+int mayorReproducciones(struct CANCION canciones[])
+{
+    int cont, mayor=0;
+    for(cont=0; cont<SIZE; cont++)
+    {
+        if(canciones[cont].numReproducciones>canciones[mayor].numReproducciones)
+            mayor=cont;
+    }
+    return(mayor);
+}
+
+
+/*1.5.Escriba la función main de su programa, en donde:
+•Declare un arreglo de canciones de 100 elementos
+•Llamea la función que captura los elementos del arreglo
+•Llame a la función que busca la canción con mas reproducciones
+•Imprima la canción con mayor número de reproducciones*/
+
 int main()
 {
-    int a = 5;
-    int b = 0;
-
-    while(a > 0){
-        int i;
-        for(i = a; i > 0; i--){
-            if(i % 2 == 0)
-                printf("!");
-            else
-                printf("*");
-        }
-        printf(" ");
-
-        int j;
-        for(j = 0; j < b; j++){
-            printf("*");
-    }
-    printf("\n");
-    a--;
-    b++;
+    int mayor;
+    struct CANCION canciones[100];
+    leerArreglo(canciones);
+    mayor=mayorReproducciones(canciones);
+    printf("La canción más reproducida en Spotify es: %s", canciones[mayor].nomCancion);
 }
-}
+
--- TAREA 0 EJERCICIO 2.c
+++ TAREA 0 EJERCICIO 2.c
@@ -1,62 +1,70 @@
 //González Medina Claudia Karina
+/*2.Dados un arreglo de N números,  realice una función que ordene el arreglo
+de forma descendente, utilizando el algoritmo de su preferencia. (Donde 0 <N <=100)*/
+
 #include <stdio.h>
-#define TM 100
+#define SIZE 100
 
-void pideSrt(char str[]);
-void cambiaPhraseMin(char str[]);
-void muestraStr(char str[], int counter);
-
-int main()
+int pideN()
 {
-    char str[TM];
-    int count;
-    pideSrt(str);
-    cambiaPhraseMin(str);
-    muestraStr(str, count);
+    int n;
+    do{
+    printf("No. of fact: ");
+    scanf("%d", &n);
+    }while(n<1||n>SIZE);
+    return(n);
 }
 
-void pideSrt(char str[])
+void pideArreglo(int array[SIZE], int n)
 {
-    printf("Give me a phrase/string: ");
-    gets(str);
-}
-
-void cambiaPhraseMin(char str[])
-{
-    int counter;
-    for(counter=0; str[counter]!='\0'; counter++)
+    int cont;
+    for(cont = 0; cont<=n; cont++)
     {
-        if(str[counter]=='a')
-                    str[counter]='A';
-        else
-            if(str[counter]=='A')
-                    str[counter]='a';
-        if(str[counter]=='e')
-                    str[counter]='E';
-        else
-        if(str[counter]=='E')
-                    str[counter]='e';
-        if(str[counter]=='i')
-                    str[counter]='I';
-        else
-            if(str[counter]=='I')
-                    str[counter]='i';
-        if(str[counter]=='o')
-                    str[counter]='O';
-        else
-            if(str[counter]=='O')
-                    str[counter]='o';
-        if(str[counter]=='u')
-                    str[counter]='U';
-        else
-            if(str[counter]=='U')
-                    str[counter]='u';
+        printf("Give me a number of fact %d: ", cont);
+        scanf("%d", &array[cont]);
     }
 }
 
-
-
-void muestraStr(char str[], int counter)
+void swap(int *xp, int *yp)
 {
-    printf("%s\n", str);
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+    int i,j;
+    for(i=1;i<n;i++)
+    {
+        for(j=0;j<n-i;j++)
+        {
+            if(arr[j]<arr[j+1])
+            {
+                swap(&arr[j],&arr[j+1]);
+            }
+        }
+    }
+
+
+}
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int arr[SIZE];
+    int n;
+    n=pideN();
+    pideArreglo(arr, n);
+    selectionSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
 }
